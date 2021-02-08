204e9d89cff52091f17be69ea76f5261a3fa0d4c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-08 00:17:23 -0500

    Update main.go

--- main.go
+++ main.go
@@ -10,12 +10,13 @@ import (
 	"time"
 )
 
-func main() {
-	// Set account keys & information
-	accountSid := "ACXXXX"
-	authToken := "XXXXXX"
-	urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
+var (
+	accountSid = "ACXXXX"
+	authToken  = "XXXXXX"
+)
 
+func main() {
+	urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
 	// Create possible message bodies
 	quotes := [7]string{"I urge you to please notice when you are happy, and exclaim or murmur or think at some point, 'If this isn't nice, I don't know what is.'",
 		"Peculiar travel suggestions are dancing lessons from God.",
@@ -24,24 +25,20 @@ func main() {
 		"That is my principal objection to life, I think: It's too easy, when alive, to make perfectly horrible mistakes.",
 		"So it goes.",
 		"We must be careful about what we pretend to be."}
-
 	// Set up rand
 	rand.Seed(time.Now().Unix())
-
 	// Pack up the data for our message
 	msgData := url.Values{}
 	msgData.Set("To", "NUMBER_TO")
 	msgData.Set("From", "NUMBER_FROM")
 	msgData.Set("Body", quotes[rand.Intn(len(quotes))])
 	msgDataReader := *strings.NewReader(msgData.Encode())
-
 	// Create HTTP request client
 	client := &http.Client{}
 	req, _ := http.NewRequest("POST", urlStr, &msgDataReader)
 	req.SetBasicAuth(accountSid, authToken)
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
 	// Make HTTP POST request and return message SID
 	resp, _ := client.Do(req)
 	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
